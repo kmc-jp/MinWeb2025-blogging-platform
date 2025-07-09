0cfc87c097b6561f82ed3d459b57573a769a0fa2
stderr0r <3st21r@gmail.com>
2025-07-09 20:23:30 +0900

    個別記事ページのデザインを変更

--- src/app/articles/[id]/page.tsx
+++ src/app/articles/[id]/page.tsx
@@ -29,16 +29,17 @@ export default function ArticlePage({ params }: { params: Promise<{ id: string }
     }
 
     return (
-        <div className="container mx-auto px-4 py-8">
+        
+        <div className="bg-white mx-4 mt-4 px-7 pt-10 pb-20 rounded-sm">
             <article className="max-w-none">
-                <div className="mb-8">
-                    <h1 className="text-4xl font-extrabold text-black mb-2">{safeStringify(article.title)}</h1>
-                    <div className="flex items-center text-gray-500">
-                        <p className="mr-4">By <Link href={`/author/${article.author.inner}`} className="hover:underline">{safeStringify(article.author)}</Link></p>
-                        <p>{new Date(safeStringify(article.created_at)).toLocaleDateString('ja-JP')}</p>
+                <div className="mb-10">
+                    <h1 className="text-4xl font-extrabold text-gray-600 mb-4">{safeStringify(article.title)}</h1>
+                    <div className="flex items-center">
+                        <p className="mr-4 text-sky-600">By <Link href={`/author/${article.author.inner}`} className="hover:underline">{safeStringify(article.author)}</Link></p>
+                        <p className="text-gray-500">{new Date(safeStringify(article.created_at)).toLocaleDateString('ja-JP')}</p>
                     </div>
                 </div>
-                <p className="text-black">{safeStringify(article.content)}</p>
+                <p className="text-gray-700">{safeStringify(article.content)}</p>
             </article>
         </div>
     );
